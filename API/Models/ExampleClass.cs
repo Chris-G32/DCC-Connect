1fea37cffe1b652a876bfdf322f8366c43151a30
ChrisG-32 <cgruska@kent.edu>
2024-09-20 11:46:12 -0400

    add: route with no functionality

---
+++ API/Models/ExampleClass.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models;
+
+
+public class ExampleClass
+{
+    public int count { get; set; } = 0;
+    public string name { get; set; } = "Example";
+}
---
+++ API/Routes/ShiftSchedulingRoutes.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using Carter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Routes;
+
+public class ShiftSchedulingRoutes : CarterModule
+{
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("test", TestRoute);
+    }
+
+    public IResult TestRoute()
+    {
+        ExampleClass eg = new();
+        return Results.Ok(eg);
+    }
+}
