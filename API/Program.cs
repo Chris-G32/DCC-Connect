9e66ba34ec066ebccae9ffdf7d44bc8c1461d07d
ChrisG-32 <cgruska@kent.edu>
2024-10-28 19:05:00 -0400

    add: simple authentication support

--- API/Program.cs
+++ API/Program.cs
@@ -7,12 +7,37 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "API", Version = "v1" });
+
+    // Add security definition
+    c.AddSecurityDefinition("basic", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "basic",
+        Description = "Enter your credentials to access the Swagger UI"
+    });
+
+    // Add security requirement
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        { new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "basic"
+                }
+            }, new string[] { }
+        }
+    });
+});
+builder.Services.AddLogging();
 builder.Services.AddCarter();
 builder.Services.AddSingleton<IMongoDBSettingsProvider, MongoDBSettingsProvider>();
 builder.Services.AddSingleton<IDatabaseInitializer, DatabaseInitializer>();
 builder.Services.AddSingleton<IDBClientProvider, MongoClientProvider>();
-builder.Services.AddSingleton<IDBTransactionService, DBTransactionService>();
 builder.Services.AddSingleton<IDatabaseProvider, DatabaseProvider>();
 builder.Services.AddSingleton<ICollectionsProvider, CollectionsProvider>();
 builder.Services.AddSingleton<IAvailabiltyService, AvailablityService>();
